7caa39721d803ae4a63a4a9cb88506ae33b6013a
dudu2599 <eduardo.2599@hotmail.com>
2018-05-06 20:35:35 -0300

    Criado o controller da auditoria

---
+++ app/Http/Controllers/Auditoria/AuditoriaController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Auditoria;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class AuditoriaController extends Controller
+{
+    //
+}
