dc4668f816b7ce395ea292a854426ad43f11d277
dudu2599 <eduardo.2599@hotmail.com>
2018-05-14 11:42:04 -0300

    Adicionado um seeder para as disciplinas.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
-
+use \App\Disciplina;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -11,6 +11,39 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call('DisciplinaTableSeeder');
     }
 }
+
+class DisciplinaTableSeeder extends Seeder {
+
+    public function run()
+    {
+        DB::table('disciplinas')->delete();
+
+        Disciplina::create([
+            'nome' => 'História',
+            'descricao'  => 'Descrição da disciplina de história',
+        ]);
+        Disciplina::create([
+            'nome' => 'Matemática',
+            'descricao'  => 'Descrição da disciplina de Matemáticas',
+        ]);
+        Disciplina::create([
+            'nome' => 'Geografia',
+            'descricao'  => 'Descrição da disciplina de Geografia',
+        ]);
+        Disciplina::create([
+            'nome' => 'Filosofia',
+            'descricao'  => 'Descrição da disciplina de Filosofia',
+        ]);
+        Disciplina::create([
+            'nome' => 'Química',
+            'descricao'  => 'Descrição da disciplina de Química',
+        ]);
+    }
+
+}
+
+
+
