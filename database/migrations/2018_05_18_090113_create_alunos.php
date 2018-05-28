b1682c01bad520a9b73805d581f014ad921cd13a
dudu2599 <eduardo.2599@hotmail.com>
2018-05-28 09:53:44 -0300

    Removido o atributo 'unique' dos id_projeto da tabela alunos.

--- database/migrations/2018_05_18_090113_create_alunos.php
+++ database/migrations/2018_05_18_090113_create_alunos.php
@@ -39,7 +39,7 @@ class CreateAlunos extends Migration
             $table->unsignedInteger('categoria_id');
             $table->foreign('categoria_id')->references('id')->on('categorias')->onDelete('cascade');
 
-            $table->unsignedInteger('projeto_id')->nullable()->unique();
+            $table->unsignedInteger('projeto_id')->nullable();
             $table->foreign('projeto_id')->references('id')->on('projetos')->onDelete('cascade');
 
             $table->timestamps();
