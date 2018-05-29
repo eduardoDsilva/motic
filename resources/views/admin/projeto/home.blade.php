0b11e4a457c2522566ad76b3ea0d717bbed6fa09
dudu2599 <eduardo.2599@hotmail.com>
2018-05-29 13:55:00 -0300

    Removida a tela com de home com cartões e substituida pela tela com a table dos projetos.

--- resources/views/admin/projeto/home.blade.php
+++ resources/views/admin/projeto/home.blade.php
@@ -1,57 +1,99 @@
+
 @extends('layout.site')
 
 @section('titulo','Motic Admin')
 
 @section('conteudo')
 
-<div class="section no-pad-bot" id="index-banner">
+    @if(session('success'))
+        {{session('success')}}
+    @endif
+
+    <a class="btn green" href="{{url()->previous()}}">Voltar</a>
+
+    <div class="section no-pad-bot" id="index-banner">
+        <div class="container">
+            <br><br>
+            <h1 class="header center orange-text">Projetos</h1>
+            <div class="row center">
+                <h5 class="header col s12 light">Essas são os projetos cadastrados no sistema!</h5>
+            </div>
+            <br>
+        </div>
+    </div>
+
     <div class="container">
-        <br><br>
-        <h1 class="header center orange-text">Administrador</h1>
-        <div class="row center">
-            <h5 class="header col s12 light">Bem vindo, {{ Auth::user()->name }}!</h5>
-        </div>
-        <br>
+        <div class="col s12 m4 l8">
 
-    </div>
-</div>
+            <table class="centered responsive-table highlight bordered">
+                <thead>
+                <tr>
+                    <th>Título</th>
+                    <th>Área</th>
+                    <th>Estande</th>
+                    <th>Resumo</th>
+                    <th>Status</th>
+                    <th>Categoria</th>
+                    <th>Escola</th>
+                </tr>
+                </thead>
+                <tbody>
+                @forelse ($projetos as $projeto)
+                    <tr>
+                        <td>{{$projeto->titulo}}</td>
+                        <td>{{$projeto->area}}</td>
+                        <td>{{$projeto->estande == null ? 'Estande não definida' : $projeto->estande}}</td>
+                        <td>{{$projeto->resumo}}</td>
+                        <td>{{$projeto->status}}</td>
+                        <td>{{$projeto->categoria->categoria}}</td>
+                        <td>{{$projeto->escola->name}}</td>
+                        <td>
+                            <a class="modal-trigger tooltipped" data-position="top" data-delay="50" data-tooltip="Editar"  href="{{ url("/admin/escola/update/".$projeto."/editar") }}"><i class="small material-icons">edit</i></a>
+                            <a data-target="modal1" class="modal-trigger tooltipped" data-position="top" data-delay="50" data-tooltip="Deletar"  href="#modal1"> <i class="small material-icons">delete</i></a>
+                        </td>
+                    </tr>
 
-<div class="container">
-    <div class="col s12 m4 l8">
-
-        <div class="row">
-            <a href="{{route ('admin/projeto/cadastro/registro')}}">
-                <div class="col s12 m6">
-                    <div class="card red darken-2">
-                        <div class="card-content black-text center-align">
-                            <i class="large material-icons">border_color</i>
+                    <!-- Modal Structure -->
+                    <div id="modal1" class="modal">
+                        <div class="modal-content">
+                            <h4>Deletar</h4>
+                            <p>Você tem certeza que deseja deletar essa escola?</p>
                         </div>
-                        <div class="card-action white-text">
-                            <span class="card-title">Cadastrar projeto</span>
-
-                            <p>Clique aqui para cadastrar um projeto no sistema.</p>
+                        <div class="modal-footer">
+                            <a href="{{ url("admin/escola/deletar/".$projeto."/excluir") }}" class="btn red">Sim</a>
                         </div>
                     </div>
-                </div>
-            </a>
+                @empty
+                    <tr>
+                        <td>Nenhuma escola encontrada</td>
+                        <td>Nenhuma escola encontrada</td>
+                        <td>Nenhuma escola encontrada</td>
+                        <td>Nenhuma escola encontrada</td>
+                        <td>Nenhuma escola encontrada</td>
+                    </tr>
+                @endforelse
+                </tbody>
+            </table>
 
-            <a href="{{route ('admin/projeto/busca/buscar')}}">
-                <div class="col s12 m6">
-                    <div class="card blue darken-2">
-                        <div class="card-content black-text center-align">
-                            <i class="large material-icons">bookmark_border</i>
-                        </div>
-                        <div class="card-action white-text">
-                            <span class="card-title">Listar projetos</span>
+            <br><br>
 
-                            <p>Clique aqui para listar os projetos.</p>
-                        </div>
-                    </div>
-                </div>
-            </a>
+            <div class="fixed-action-btn">
+                <a class="btn-floating btn-large red">
+                    <i class="large material-icons">mode_edit</i>
+                </a>
+                <ul>
+                    <li><a class="btn-floating red tooltipped" data-position="left" data-delay="50" data-tooltip="Adicionar projeto" href="{{route ('admin/projeto/cadastro/registro')}}"><i class="material-icons">add</i></a></li>
+                    <li><a class="btn-floating yellow darken-1 tooltipped" data-position="left" data-delay="50" data-tooltip="Adicionar aluno" href="{{route ('admin/aluno/cadastro/registro')}}"><i class="material-icons">format_quote</i></a></li>
+                    <li><a class="btn-floating green tooltipped" data-position="left" data-delay="50" data-tooltip="Adicionar professor" href="{{route ('admin/professor/cadastro/registro')}}"><i class="material-icons">publish</i></a></li>
+                    <li><a class="btn-floating blue tooltipped" data-position="left" data-delay="50" data-tooltip="Adicionar escola" href="{{route ('admin/escola/cadastro/registro')}}"><i class="material-icons">attach_file</i></a></li>
+                </ul>
+            </div>
+
+            <br><br>
+
+            <br><br>
+
         </div>
     </div>
-</div>
 
 @endsection
-
