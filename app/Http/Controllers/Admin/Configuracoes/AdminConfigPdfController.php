53bd63e91377c316dd270e61f9778d510da48197
dudu2599 <eduardo.2599@hotmail.com>
2018-07-16 10:38:51 -0300

    Desenvolvida a homologação do upload de termos.

--- app/Http/Controllers/Admin/Configuracoes/AdminConfigPdfController.php
+++ app/Http/Controllers/Admin/Configuracoes/AdminConfigPdfController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Auditoria\AuditoriaController;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Auth;
 
 class AdminConfigPdfController extends Controller
 {
@@ -53,6 +54,9 @@ class AdminConfigPdfController extends Controller
                 $extensao = $request->pdf->extension();
                 $nameFile = "regras-motic.$extensao";
                 $request->pdf->storeAs('regras', $nameFile);
+                $texto = 'O usuário '.Auth::user()->username.' fez o upload do pdf regras-motic.';
+                $texto = str_replace(",", ", ", json_encode($texto, JSON_UNESCAPED_UNICODE));
+                $this->auditoriaController->storeCreate($texto, 999, 'upload');
                 return redirect()->back();
             }
 
@@ -68,6 +72,9 @@ class AdminConfigPdfController extends Controller
                 $extensao = $request->pdf->extension();
                 $nameFile = "regulamento-motic.$extensao";
                 $request->pdf->storeAs('regulamentos', $nameFile);
+                $texto = 'O usuário '.Auth::user()->username.' fez o upload do pdf regulamento-motic.';
+                $texto = str_replace(",", ", ", json_encode($texto, JSON_UNESCAPED_UNICODE));
+                $this->auditoriaController->storeCreate($texto, 999, 'upload');
                 return redirect()->back();
             }
 
@@ -83,6 +90,9 @@ class AdminConfigPdfController extends Controller
                 $extensao = $request->pdf->extension();
                 $nameFile = "termo-menor-motic.$extensao";
                 $request->pdf->storeAs('termos', $nameFile);
+                $texto = 'O usuário '.Auth::user()->username.' fez o upload do pdf termo-menor-motic.';
+                $texto = str_replace(",", ", ", json_encode($texto, JSON_UNESCAPED_UNICODE));
+                $this->auditoriaController->storeCreate($texto, 999, 'upload');
                 return redirect()->back();
             }
 
@@ -98,6 +108,9 @@ class AdminConfigPdfController extends Controller
                 $extensao = $request->pdf->extension();
                 $nameFile = "termo-maior-motic.$extensao";
                 $request->pdf->storeAs('termos', $nameFile);
+                $texto = 'O usuário '.Auth::user()->username.' fez o upload do pdf termo-maior-motic.';
+                $texto = str_replace(",", ", ", json_encode($texto, JSON_UNESCAPED_UNICODE));
+                $this->auditoriaController->storeCreate($texto, 999, 'upload');
                 return redirect()->back();
             }
 
@@ -113,6 +126,9 @@ class AdminConfigPdfController extends Controller
                 $extensao = $request->pdf->extension();
                 $nameFile = "contrato-convivencia-motic.$extensao";
                 $request->pdf->storeAs('termos', $nameFile);
+                $texto = 'O usuário '.Auth::user()->username.' fez o upload do pdf contrato-convivencia-motic.';
+                $texto = str_replace(",", ", ", json_encode($texto, JSON_UNESCAPED_UNICODE));
+                $this->auditoriaController->storeCreate($texto, 999, 'upload');
                 return redirect()->back();
             }
 
@@ -128,6 +144,9 @@ class AdminConfigPdfController extends Controller
                 $extensao = $request->pdf->extension();
                 $nameFile = "ficha-de-avaliacao-motic.$extensao";
                 $request->pdf->storeAs('termos', $nameFile);
+                $texto = 'O usuário '.Auth::user()->username.' fez o upload do pdf ficha-de-avaliacao-motic.';
+                $texto = str_replace(",", ", ", json_encode($texto, JSON_UNESCAPED_UNICODE));
+                $this->auditoriaController->storeCreate($texto, 999, 'upload');
                 return redirect()->back();
             }
 
