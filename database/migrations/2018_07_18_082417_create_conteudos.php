e3e513d8964f203d651e9f01fbd03adbeac17d06
dudu2599 <eduardo.2599@hotmail.com>
2018-07-18 13:48:32 -0300

    Adicionado o frameworkd CKeditor para o gerênciamento da home-page pelo administrador.

--- database/migrations/2018_07_18_082417_create_conteudos.php
+++ database/migrations/2018_07_18_082417_create_conteudos.php
@@ -15,15 +15,8 @@ class CreateConteudos extends Migration
     {
         Schema::create('conteudos', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('sobre')->nullable();
-            $table->string('local')->nullable();
-            $table->string('quando')->nullable();
-            $table->string('avaliacao')->nullable();
-            $table->string('quem_pode_participar')->nullable();
-            $table->string('jogos_motic')->nullable();
-            $table->string('responsaveis')->nullable();
-            $table->string('telefone_contato')->nullable();
-            $table->string('email_contato')->nullable();
+            $table->longText('sobre', 100000)->nullable();
+            $table->longText('contato', 100000)->nullable();
             $table->timestamps();
         });
     }
