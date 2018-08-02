d01f392c0ae3d63443e779afc378459300dc99a6
dudu2599 <eduardo.2599@hotmail.com>
2018-08-02 10:35:07 -0300

    Alterado o texto de "loggout" para "sair".

--- resources/views/admin/nav/nav.blade.php
+++ resources/views/admin/nav/nav.blade.php
@@ -199,7 +199,7 @@
 <li class="white">
     <a class="collapsible-header" href="{{ route('logout') }}" onclick="event.preventDefault();
                                         document.getElementById('logout-form').submit();"> <i
-                class="small material-icons">exit_to_app</i>Logout
+                class="small material-icons">exit_to_app</i>Sair
     </a>
 
     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
